482fe20cc40436fb9a985e0a4b2b23822f852b25
DuckJeans <lemonfox223@gmail.com>
2026-03-18 19:48:04 +0900

    (2026.3.18) 비트

--- Program/Source.c
+++ Program/Source.c
@@ -1,102 +1,127 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-#pragma region 변수
-	//데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+#pragma region 산술 연산자
+
+	// 1. 변수 = 변수 + 변수
+
+	// 2. 변수 = 변수 - 리터럴 상수
+
+	// 3. 변수 = 변수 * 심볼릭 상수
+
+	// 4. 변수 = 리터럴 상수 / 리터럴 상수
+
+	// 5. 변수 = 심볼릭 상수 % 리터럴 상수
+
+	//int a = 5;
+	//
+	//int b = 2;
+	//1.
+	//printf("%d = %d + %d\n", a + b, a, b);
+	//
+	//b = 20;
+	//2.
+	//printf("%d = %d - %d\n", a - b, a, b);
+	//
+	//const int c = 10;
+	//3.
+	//printf("%d = %d * %d\n", a * c, a, c);
+	//
+	//a = 5;
+	//b = 5;
+	//4.
+	//printf("%d = %d / %d\n", a / b, a, b);
+	//
+	//a = 10;
+	//5.
+	//printf("%d\n", c % a);
+
 
 #pragma endregion
 
-#pragma region 자료형
-	//데이터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+#pragma region 비트
 
-	/*자료형은 각각의 자료형마다 크기가 정해져 있으며, 자료형의
-	크기는 바이트 단위로 이루어져 있으며, 자료형의 경우 자료형에
-	따라 저장할 수 있는 값의 종류와 범위가 결정됩니다.*/
+	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의
+	// 조합으로 논리 계산을 수행하는 단위입니다.
 
+#pragma endregion
 
+#pragma region (10)진수를 (2)진수로 변환하는 과정
+	// 10진수를 1이 될 때까지 계속 2로 나누어 준 다음
+	// 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬합니다.
+
+	// 23을 이진수로 나타내어라
+	// 23 -> 10111
+
+#pragma endregion
+
+#pragma region (2)진수를 (10)진수로 변환하는 과정
+	// 1byte에 2진수로 저장된 값을 2의 제곱으로 나타냅니다.
+
+	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한 다음
+	// 각각의 비트를 모두 더하여 10진수로 나타냅니다.
+
+#pragma endregion
+
+#pragma region 비트 연산자
+	// 비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자입니다.
 
 
 #pragma endregion
 
-#pragma region 변수의 이름 규칙
+#pragma region AND 연산자
+	// 두개의 피 연산자가 모두 1이면 1을 반환하는 연산자입니다.
 
-	// 1. 변수의 이름은 대소문자를 구분합니다.
-	// ex) int health;
-	// ex) int HEALTH;
+	//int wisdom = 10;
+	//int dexterity = 6;
+	//
+	//printf("wisdom & dexterity : %d\n", wisdom & dexterity);
+#pragma endregion
 
-	// 2. 변수의 이름으로 예약어를 사용할 수 없습니다.
-	// ex) char char;
+#pragma region OR 연산자
+	// 두 개의 피연산자 중에 하나라도 1이 있다면 1을 반환하는 연산자입니다.
 
-	// 3. 변수의 이름에는 공백이 포함 될 수 없습니다.
-	// ex) float time Delta;
-
-	// 4. 변수의 이름으로 특수 문자는 ", _ ,$ 만 사용할 수 있습니다.
-	// ex) long currency$;
-	// ex) short under_score;
-
-	// 5. 변수의 이름은 숫자로 시작할 수 없습니다.
-	// ex) int 5days;
-	// ex) int days5;
-
-#pragma region 서식 지정자
-	// 변수의 값을 출력하기 위해 자료형의 정보를 명시적으로
-	// 지정해주는 것입니다.
-
-	int integer = 10;
-
-	char character = 'A';
-
-	float decimal = 5.5f;
-
-	// 서식 지정자의 경우 하나의 표준 출력 함수에 여러개의
-	// 서식을 넣을 수 있으며, 서식 지정자의 순서는 왼쪽에서
-	// 부터 시작합니다.
-
-	printf("character : %c \ninteger : %d \ndecimal : %f", character, integer, decimal);
-
-	// 서식 지정자는 각각의 서식에 따라 원하는 데이터를 출력
-	// 할 수 있으며, 서식과 변수의 형태가일치하지 않으면
-	// 원하는 값이 출력되지 않습니다.
-	   
-	printf("\n===============================\n");
-
-	integer = 25;
-
-	character = 'B';
-
-	decimal = 10.2f;
-
-	printf("character : %c \n", character);
-	printf("integer : %d \n", integer);
-	printf("float : %f \n", decimal);
-
-	// 변수의 메모리 공간을 프로그램이 실행될 때마다 바뀌며, 여러
-	// 개의 변수가 있을 때 서로 고류의 메모리 공간을 가지고 있습니다.
+	//int posion = 8;		   //1000
+	//int freeze = 12;		   //1100
+	//						   //1000 = 8
+	//printf("posion | freeze : %d", posion | freeze);
 
 #pragma endregion
 
+#pragma region XOR 연산자
+	// 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면
+	// 1을 반환하는 연산자입니다.
+
+	//int idle = 15; 			//1111
+	//int battle = 6;			//0110
+	//						//1001 = 9
+	//printf("idle ^ battle : %d", idle ^ battle);
 #pragma endregion
 
-#pragma region 상수
+#pragma region NOT 연산자
+	// 하나의 비트를 반전시키는 연산자입니다.
 
-	// 프로그램이 실행되는 동안 더 이상 값을 변경할
-	// 수 없는 메모리 공간입니다.
+	//int status = 13;      //0000 1101
+	//					  //1111 0010
+	//printf("~status : %d", ~status);
 
-	const float pi = 3.141592f;
-
-	// 상수의 경우 메모리 공간을 가지고 있지 않는 상수를
-	// 리터럴 상수라고 하며, 메모리 공간을 가지고 있는
-	// 상수를 심볼릭 상수라고 합니다.
-
-	// ex) pi = 6.125f;
-
-	printf("상수의 값 : %f\n", pi);
-
-	// 상수는 메모리 공간을 생성하는 동시에 초기화해야 하며,
-	// 한 번 저장된 
+	// 첫 번째 비트는 부호를 나타내며, 첫 번째 비트에
+	// 1이 있다면 값은 음수가 됩니다.
 
 #pragma endregion
 
+#pragma region 시프트 연산자
+
+	// 비트를 특정한 위치로 정해진 수 만큼 이동시키는 연산자입니다.
+
+	int packtet = 5;     //0000 0101
+
+	printf("packtet << 1: %d\n", packtet << 1); //0000 1010
+	printf("packtet << 2: %d\n", packtet << 2); //0001 0100
+
+	printf("packtet >> 1: %d\n", packtet >> 1); //0000 0010
+	printf("packtet >> 2: %d\n", packtet >> 2); //0000 0001
+#pragma endregion
 
 }
